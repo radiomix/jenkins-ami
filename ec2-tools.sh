0ac138d0103a525b4299c820df9c2f95ee387236
Michael Klöckner (hp01) <mkl@im7.de>
2015-03-18 15:19:02 +0100

    AMI tools  install streched to multiline apt-get updated

--- ec2-tools.sh
+++ ec2-tools.sh
@@ -7,4 +7,5 @@ echo "deb-src http://us.archive.ubuntu.com/ubuntu/ trusty multiverse" >> /etc/ap
 echo "deb http://us.archive.ubuntu.com/ubuntu/ trusty-updates multiverse" >> /etc/apt/sources.list
 echo "deb-src http://us.archive.ubuntu.com/ubuntu/ trusty-updates multiverse" >> /etc/apt/sources.list
 apt-get update
-apt-get install -y --force-yes ec2-api-tools ec2-ami-tools
+apt-get install -y --force-yes ec2-api-tools 
+apt-get install -y --force-yes ec2-ami-tools
