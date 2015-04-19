98db08b30df27ca0ff9d931726b43bb79cf6ec42
Ubuntu <ubuntu@ip-172-31-1-101.us-west-2.compute.internal>
2015-04-19 15:22:10 +0000

    download/unbundle/copy bundle, create snapshot, register ami LONG WAY

--- convert-instance-to-ebs.sh
+++ convert-instance-to-ebs.sh
@@ -26,34 +26,14 @@ aws_access_key=$AWS_ACCESS_KEY
 # secrete key from env variable, needed for authentification
 aws_secret_key=$AWS_SECRET_KEY
 
-# base AMI the instance was launched off, needed to get kernel, virtual. type etc
-aws_ami_id=ami-bdab868d
-
-# device and mountpoint of the new volume, we put our new AMI onto this device(aws_volume)
-aws_device=/dev/xvdx
-aws_mount_point=/mnt/ebs
+# device and mountpoint of the new volume; we put our new AMI onto this device(aws_volume)
+aws_ebs_device=/dev/xvdx
+aws_ebs_mount_point=/mnt/ebs
 
 # descriptions
 aws_snapshot_description="Intermediate AMI snapshot, to be deleted after completion"
 date=$(date)
 aws_ami_name="Ubuntu LTS 12.04 Jenkins-Server as of $date"
-
-
-# region
-aws_region=$AWS_REGION
-if [[ "$aws_region" == "" ]]; then
-  echo " ERROR: No AWS_REGION given!! "
-  return -2
-fi
-echo "Using region: $aws_region"
-
-# architecture
-aws_architecture=$AWS_ARCHITECTURE
-if [[ "$aws_architecture" == "" ]]; then
-  echo " ERROR: No AWS_ARCHITECTURE given!! "
-  return -3
-fi
-echo "Using region: $aws_region"
 
 # aws availability zone
 aws_avail_zone=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/)
@@ -61,92 +41,123 @@ aws_avail_zone=$(curl -s http://169.254.169.254/latest/meta-data/placement/avail
 ## instance id
 aws_instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id/)
 
+# base AMI the instance was launched off, needed to get kernel, virtual. type etc
+aws_ami_id=$AWS_AMI_ID
+if [[ "$aws_ami_id" == "" ]]; then
+  echo " ERROR: No AWS_AMI_ID given!! "
+  exit -1
+fi
+echo "Using AMI id :$aws_ami_id"
+
+# region
+aws_region=$AWS_REGION
+if [[ "$aws_region" == "" ]]; then
+  echo " ERROR: No AWS_REGION given!! "
+  exit -2
+fi
+echo "Using region: $aws_region"
+
+# architecture
+aws_architecture=$AWS_ARCHITECTURE
+if [[ "$aws_architecture" == "" ]]; then
+  echo " ERROR: No AWS_ARCHITECTURE given!! "
+  exit -3
+fi
+echo "Using: architechture: $aws_architecture"
+
+
+# x509 cert/pk file
+if [[ "$AWS_PK_PATH" == "" ]]; then
+  echo " ERROR: X509 private key file \"$AWS_PK_PATH\" not found!! "
+  exit -21
+fi
+if [[ "$AWS_CERT_PATH" == "" ]]; then
+  echo " ERROR: X509 cert key file \"$AWS_CERT_PATH\" not found!! "
+  exit -22
+fi
+
+####TODO ### check for proper S3 bucket and manifest file
+# AWS S3 Bucket 
+s3_bucket=$AWS_S3_BUCKET
+manifest=$AWS_MANIFEST
+
+
 ## config variables
 
 ######################################
 ## creating ebs volume to be bundle root dev
 command=$(sudo -E $EC2_AMITOOL_HOME/bin/ec2-create-volume --size 10 --region $aws_region --availability-zone $aws_avail_zone)
 rest=${command/VOLUME/""}
-aws_volume_id=${rest:0:12}
+aws_volume_id=${rest:0:13}
+echo "AWS-Volume created:$aws_volume_id"
 
 ######################################
 ## attache volume
-sudo -u $EC2_AMITOOL_HOME/bin/ec2-attache-volume $aws_volume_id -i $aws_instance_id --device /dev/xvdx --region $aws_region
+sudo -u $EC2_AMITOOL_HOME/bin/ec2-attache-volume $aws_volume_id -i $aws_instance_id --device $aws_ebs_device --region $aws_region
 
+######################################
+## download and unbundle instance store based AMI
 mkdir $bundle_dir
+$EC2_AMITOOL_HOME/bin/ec2-download-bundle -b  $s3_bucket -m $manifest  -a $AWS_ACCESS_KEY -s $AWS_SECRET_KEY --privatekey $AWS_PK_PATH -d $bundle_dir
 
-####TODO we are at step 6 of docu!
-# download bundle, unbundle dd to attached volume,  . . . 
+cd $bundle_dir
+$EC2_AMITOOL_HOME/bin/ec2-unbundle -m $manifest --privatekey $AWS_PK_PATH 
 
+######################################
+## copy image to EBS volume
+sudo dd if=$bundle_dir/image of=$aws_ebs_device bs=1M
+sudo partprobe $aws_ebs_device
+
+######################################
+## list block devices, create a mount point, mount EBS volume
+lsblk
+sudo mkdir $aws_ebs_mount_point
+sudo mount $aws_ebs_device $aws_ebs_mount_point
+
+######################################
+## edit /etc/fstab to remove ephimeral partitions
+echo "Edit /etc/fstab to remove ephimeral partitions"
+grep ephimeral /etc/fstab
+sleep 5
+sudo vi /etc/fstab
+
+######################################
+## unmount EBS volume
+sudo umount $aws_ebs_volume
+$EC2_AMITOOL_HOME/bin/ec2-detach-volume $aws_volume_id --region $aws_region
 
 #######################################
 ## create a snapshot and verify it
-aws_snapshot_id=$(ec2-create-snapshot --region $aws_region -d $aws_snapshot_description -O $AWS_ACCESS_KEY -W $AWS_SECRET_KEY $aws_volume_id)
-#### FIXME get the snapshot-id from command output as $aws_snapshot_id
-result=$(ec2-describe-snapshots --region $aws_region $aws_snapshot_id)
-#### FEXME get last argument: 100%
+command=$($EC2_AMITOOL_HOME/bin/ec2-create-snapshot --region $aws_region -d $aws_snapshot_description -O $AWS_ACCESS_KEY -W $AWS_SECRET_KEY $aws_volume_id)
+rest=${command/VOLUME/""}
+aws_snapshot_id=${rest:0:13}
+echo "Created snapshot:$aws_snapshot_id"
 
+#######################################
+## wait until snapshot is compleeted
+completed=""
+while [[ "$completed" == "" ]]
+do
+    completed=$($EC2_AMITOOL_HOME/bin/ec2-describe-snapshot --region $aws_region $aws_snapshot_id | grep completed)
+    echo -n ". "
+    sleep 3
+done 
 
 #######################################
 ## get processor architecture, virtualization type, and the kernel image (aki) 
-aws_achitecture=$(ec2-describe-images --region $aws_region $aws_ami_id)
+## we might not get the proper description from the base image, if it was bundled
+## without a description 
+#command=$($EC2_AMITOOL_HOME/bin/ec2-describe-images --region $aws_region $aws_ami_id)
+## but we can select a proper kernel for our region.
+source select_pvgrub_kernel.s
 
 #######################################
 ## register a new AMI from the snapshot
-aws_registerd_ami_id=$(ec2-register --region $aws_region -n $aws_ami_name -s $aws_snapshot_id -a x86_64)
-#### FIXME get the new registerd ami-id from command output as $aws_registerd_ami_id
+aws_registerd_ami_id=$($EC2_AMITOOL_HOME/bin/ec2-register -O $AWS_ACCESS_KEY -W $AWS_SECRET_KEY --region $aws_region -n $aws_ami_name -s $aws_snapshot_id -a $AWS_ARCHITECTURE --kernel $AWS_KERNEL)
  
 
 #######################################
 ## and delete the volume
 ## ec2-delete-volume $aws_volume_id 
 #######################################
-
-# AWS S3 Bucket 
-s3_bucket="im7-ami/images/copied"
-
-# x509 cert/pk file
-if [[ "$AWS_PK_PATH" == "" ]]; then
-  echo " ERROR: X509 private key file \"$AWS_PK_PATH\" not found!! "
-  return -21
-fi
-if [[ "$AWS_CERT_PATH" == "" ]]; then
-  echo " ERROR: X509 cert key file \"$AWS_CERT_PATH\" not found!! "
-  return -22
-fi
-
-mkdir /tmp/bundle
-
-
-
-####TODO
-#### check for proper S3 bucket name!
-
-
-#######################################
-### this is bundle-work
-sudo -E $EC2_HOME/bin/ec2-version
-echo "*** Bundleing AMI, this may take several minutes "
-set -x
-sudo -E $EC2_AMITOOL_HOME/bin/ec2-bundle-vol -k $AWS_PK_PATH -c $AWS_CERT_PATH -u $AWS_ACCOUNT_ID -r x86_64 -e /tmp/cert/ -d $bundle_dir -p $prefix$date_fmt  $blockDevice $partition --batch
-##TODO adjust ami name to ec2-bundle-vol command
-echo "*** Uploading AMI bundle to $s3_bucket "
-ec2-upload-bundle -b $s3_bucket -m $bundle_dir/$prefix$date_fmt.manifest.xml -a $AWS_ACCESS_KEY -s $AWS_SECRET_KEY --region $aws_region
-## only ec2-register needs jre installed!
-echo "*** Registering images"
-ec2-register   $s3_bucket/$prefix$date_fmt.manifest.xml $virtual_type -n "$aws_ami_name" -O $AWS_ACCESS_KEY -W $AWS_SECRET_KEY --region $aws_region --architecture $aws_architecture 
-set +x
-echo "*** "
-echo "*** PARAMETER USED:"
-echo "*** Root device:"$root_device
-echo "*** Grub version:"$(grub --version)
-echo "*** Bundle folder:"$bundle_dir
-echo "*** Block device mapping:"$blockDevice
-echo "*** Partition flag:"$partition
-echo "*** Virtualization:"$virtual_type
-echo "*** S3 Bucket:"$s3_bucket
-echo "*** Region:"$aws_region
-echo "*** AMI name:"$aws_ami_name
-echo "*** "
-echo "*** FINISHED BUNDLING THE AMI"
 
