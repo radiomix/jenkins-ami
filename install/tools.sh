74471bfc7cf68a1fadf750847b3a3fceef158b19
Michael Klöckner (hp01) <mkl@im7.de>
2015-05-08 19:33:15 +0200

    install python golang

--- install/tools.sh
+++ install/tools.sh
@@ -5,7 +5,7 @@
 echo ***INSTALLING TOOL
 sleep 2
 sudo apt-get update
-sudo apt-get install -y --force-yes curl wget git mosh gcc make lynx unzip docker.io build-essential
+sudo apt-get install -y --force-yes curl wget git mosh gcc make lynx unzip docker.io build-essential phyton golang
 which lynx mosh git vi wget unzip docker
 echo ***INSTALLING JAVA 
 sleep 2
