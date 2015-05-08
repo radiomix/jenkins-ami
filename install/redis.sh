1e31aa1ce713ebf1b7d8a0b832241319c83d3755
Michael Klöckner (hp01) <mkl@im7.de>
2015-05-08 20:32:35 +0200

    bug fix: use /tmp during redis installation time

--- install/redis.sh
+++ install/redis.sh
@@ -2,14 +2,15 @@
 #
 # install redis as of http://redis.io/topics/quickstart
 #
-
+apt-get install -y wget make gcc unzip
 echo "***INSTALLING redis"
-cd /usr/local/
+cd /tmp/
 wget http://download.redis.io/redis-stable.tar.gz
-sudo tar xvzf redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
 cd redis-stable
 echo "***MAKING redis"
 sudo make
 sudo make test
 sudo make install
-sudo rm redis-stable.tar.gz
+cd /tmp/
+sudo rm -rf redis-stable*
