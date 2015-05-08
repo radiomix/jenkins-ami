acaddac044b498102fac3bb1defde2063da770af
Michael Klöckner (hp01) <mkl@im7.de>
2015-05-08 20:10:38 +0200

    add sleep command to redis installation, install redis at earlear stage

--- install/redis.sh
+++ install/redis.sh
@@ -6,9 +6,10 @@
 echo "***INSTALLING redis"
 cd /usr/local/
 wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
+sudo tar xvzf redis-stable.tar.gz
 cd redis-stable
-sudo make 
+echo "***MAKING redis"
+sudo make
 sudo make test
 sudo make install
-
+sudo rm redis-stable.tar.gz
--- install/tools.sh
+++ install/tools.sh
@@ -3,6 +3,8 @@
 # install tools needed for redis, jenkins, rabitmq
 #
 echo ***INSTALLING TOOL
+lsblk
+df -h
 sleep 2
 sudo apt-get update
 sudo apt-get install -y --force-yes curl wget git mosh gcc make lynx unzip docker.io build-essential phyton golang
@@ -16,3 +18,5 @@ sleep 2
 sudo apt-get install -y ruby-dev rake exuberant-ctags ack-grep
 #cd
 #curl -Lo- https://bit.ly/janus-bootstrap | bash
+df -h
+sleep 2
