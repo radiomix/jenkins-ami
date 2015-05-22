d0f2cebf24b56f8d5dfec3eed134f68cb27cc859
Michael Klöckner (hp01) <mkl@im7.de>
2015-05-22 14:27:17 +0200

    link etcd binaries to /usr/local/bin

--- install/etcd.sh
+++ install/etcd.sh
@@ -9,8 +9,10 @@ echo "***INSTALLING ETCD version $version"
 cd /usr/local
 curl -L  https://github.com/coreos/etcd/releases/download/$version/etcd-$version-linux-amd64.tar.gz -o etcd-$version-linux-amd64.tar.gz
 tar xzvf etcd-$version-linux-amd64.tar.gz
-ln -v -s etcd-$version-linux-amd64/etcd /usr/local/bin
-ln -v -s etcd-$version-linux-amd64/etcdctl /usr/local/bin/
-#which etcd
-#which etcdctl
+ln -v -s /usr/local/etcd-$version-linux-amd64/etcd /usr/local/bin
+ln -v -s /usr/local/etcd-$version-linux-amd64/etcdctl /usr/local/bin/
+#
+export PATH=$PATH:/usr/local/bin
+which etcd
+which etcdctl
 
