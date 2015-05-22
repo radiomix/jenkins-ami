ce19e3767f1591fdf3a897e480c897616a040e89
Michael Klöckner (hp01) <mkl@im7.de>
2015-05-22 13:36:55 +0200

    add script to install etcd

---
+++ install/etcd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# install etcd as of https://github.com/coreos/etcd/releases/tag/$version
+#
+
+version="v2.0.11"
+echo "***INSTALLING ETCD version $version"
+# Install ETCD
+cd /usr/local
+curl -L  https://github.com/coreos/etcd/releases/download/$version/etcd-$version-linux-amd64.tar.gz -o etcd-$version-linux-amd64.tar.gz
+tar xzvf etcd-$version-linux-amd64.tar.gz
+ln -s etcd-$version-linux-amd64/etcd /usr/local/bin
+ln -s etcd-$version-linux-amd64/etcdctl /usr/local/bin/
+#which etcd
+#which etcdctl
+
