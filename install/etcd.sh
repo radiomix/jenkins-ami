e0f2de69159b1f828e6e55f33555ea61c6230a2a
Michael Klöckner (hp01) <mkl@im7.de>
2015-05-23 19:59:31 +0200

    add sudo to commands, copy /etc/default/etcd and /etc/init.d/etcd, link start/stop scripts

--- install/etcd.sh
+++ install/etcd.sh
@@ -8,11 +8,28 @@ echo "***INSTALLING ETCD version $version"
 # Install ETCD
 cd /usr/local
 curl -L  https://github.com/coreos/etcd/releases/download/$version/etcd-$version-linux-amd64.tar.gz -o etcd-$version-linux-amd64.tar.gz
-tar xzvf etcd-$version-linux-amd64.tar.gz
-ln -v -s /usr/local/etcd-$version-linux-amd64/etcd /usr/local/bin
-ln -v -s /usr/local/etcd-$version-linux-amd64/etcdctl /usr/local/bin/
-#
+sudo tar xzvf etcd-$version-linux-amd64.tar.gz
+# link binaries
+sudo ln -v -s /usr/local/etcd-$version-linux-amd64/etcd /usr/local/bin
+sudo ln -v -s /usr/local/etcd-$version-linux-amd64/etcdctl /usr/local/bin/
+
+# copy default/init script to the right place
+sudo mv /tmp/etcd.init /etc/init.d/etcd
+sudo chown root.root /etc/init.d/etcd
+sudo mv /tmp/etcd.default /etc/default/etcd
+sudo chown root.root /etc/default/etcd
+
+# link start/stop scripts
+cd /etc/rc5.d/
+sudo ln -v -s ../init.d/etcd S19etcd
+cd /etc/rc2.d/
+sudo ln -v -s ../init.d/etcd S19etcd
+
+# check installation
 export PATH=$PATH:/usr/local/bin
+service etcd start
 which etcd
 which etcdctl
+#
+#
 
